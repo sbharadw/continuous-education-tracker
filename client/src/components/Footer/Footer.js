0e1fdfe34d8a29d1cee6cde904450d6a361f1dc4
Hatice <haticeevci@gmail.com>
2021-04-24 21:22:14 -0400

    logo added top header

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,10 +4,10 @@ import { Container } from "react-bootstrap";
 class Footer extends Component {
   render() {
     return (
-      <footer bg="dark" className="footer ">
+      <footer className="footer">
         <Container fluid>
           {/* <!-- Copyright --> */}
-          <div class="col-md-12 col-sm-12 col-xs-12 text-center py-2  text-dark">
+          <div class="col-md-12 col-sm-12 col-xs-12 text-center py-1  text-dark">
             © 2021 Copyright:TeamHulk
           </div>
           {/* <!-- End Copyright --> */}
--- client/src/components/login-button.js
+++ client/src/components/login-button.js
@@ -25,7 +25,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button class="btn btn-light" onClick={() => loginWithRedirect()}>Log In</button>;
+  return <button class="btn btn-light btn-sm" onClick={() => loginWithRedirect()}>Log In</button>;
 };
 
 export default LoginButton;
